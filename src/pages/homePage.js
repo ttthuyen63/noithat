8857751bfe512497590c1fccfac9700df2fab602
ttthuyen63 <ttthuyen6311@gmail.com>
2023-10-12 03:55:22 +0700

    update

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -85,7 +85,7 @@ export default function HomePage() {
                   navigate("/");
                 }}
               >
-                Thoát
+                Đăng xuất
               </Button>
             </h5>
           </div>
